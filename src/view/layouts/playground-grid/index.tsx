2b0320827881f11235190a63ebe152cdc52fdaab
Vladimir <wlashcontact@gmail.com>
2022-09-30 21:40:17 +0300

    custom playground size added

--- src/view/layouts/playground-grid/index.tsx
+++ src/view/layouts/playground-grid/index.tsx
@@ -6,20 +6,25 @@ import { useId } from "react";
 interface PlaygroundGridProps extends Playground {
   onCellClick?: (position: number) => void,
   sellSize?: number,
+  spacing?: number,
 }
 
 export const PlaygroundGridLayout = (props: PlaygroundGridProps) => {
 
-  const {rank, data, sellSize = 120} = props
+  const {rank, data, sellSize = 120, spacing = 5, onCellClick} = props
 
   const playgroundId = useId()
 
-  return <SimpleGrid cols={rank}>
+  return <SimpleGrid cols={rank} spacing={spacing}>
     {
-      data.map(cell => <PlaygroundCell
-        key={`${playgroundId}/${cell.position}`}
-        mark={cell.mark}
-        size={sellSize}/>
+      data.map((cell) => <PlaygroundCell
+          key={`${playgroundId}/${cell.position}`}
+          mark={cell.mark}
+          size={sellSize}
+          onClick={() => {
+            onCellClick && onCellClick(cell.position)
+          }}
+        />
       )
     }
   </SimpleGrid>
--- src/view/pages/playground/index.tsx
+++ src/view/pages/playground/index.tsx
@@ -1,11 +1,134 @@
-import { PlaygroundCell } from "../../components";
-import { Mark } from "../../../data";
+import {
+  Button,
+  Card,
+  Center,
+  Container,
+  Stack,
+  Text,
+  Progress,
+  Group,
+  ActionIcon,
+  NumberInput,
+  NumberInputHandlers
+} from "@mantine/core";
+import { useListState } from "@mantine/hooks";
+import { PlaygroundGridLayout } from "../../layouts";
+import { Cell, Mark } from "../../../data";
+import { Refresh } from "tabler-icons-react";
+import { useEffect, useRef, useState } from "react";
+
 
 function PlaygroundPage () {
 
-  return <>
+  const ROW_MAX_HEIGHT = 180 * 3
 
-  </>
+  const [gameRank, setGameRank] = useState(3)
+  const handlers = useRef<NumberInputHandlers>()
+
+  const getInitialPlayground = (gameRank: number) => [...Array(gameRank * gameRank)]
+    .map((element, index) => ({
+      position: index,
+      mark: Mark.VOID,
+      protected: false,
+    }))
+
+  const [playgroundCells, playgroundHandlers] = useListState<Cell>(getInitialPlayground(gameRank))
+
+
+  useEffect(() => {
+    playgroundHandlers.setState(getInitialPlayground(gameRank))
+  }, [gameRank])
+
+
+  const onIncrementSize = () => handlers.current?.increment()
+
+  const onDecrementSize = () => handlers.current?.decrement()
+
+  const onCellClick = (index: number) => {
+    playgroundHandlers.setItem(index, {
+      position: index,
+      mark: Mark.CROSS,
+      protected: true,
+    })
+  }
+
+  const onResetClick = () => {
+    playgroundHandlers.setState(getInitialPlayground(gameRank))
+  }
+
+
+  return <Container>
+    <Stack align={'center'}>
+      <Group position='center' grow>
+        <Card
+          withBorder
+          radius="md"
+          p="xl"
+          sx={(theme) => ({
+            backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
+          })}
+        >
+          <Text size="xs" transform="uppercase" weight={700} color="dimmed">
+            Monthly goal
+          </Text>
+          <Text size="lg" weight={500}>
+            $5.431 / $10.000
+          </Text>
+          <Progress value={54.31} mt="md" size="lg" radius="xl" />
+        </Card>
+        <Card
+          withBorder
+          radius="md"
+          p="xl"
+          sx={(theme) => ({
+            backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
+          })}
+        >
+          <Text size="xs" transform="uppercase" weight={700} color="dimmed">
+            Monthly goal
+          </Text>
+          <Text size="lg" weight={500}>
+            $5.431 / $10.000
+          </Text>
+          <Progress value={54.31} mt="md" size="lg" radius="xl" />
+        </Card>
+      </Group>
+
+      <Center mb={'lg'}>
+        <PlaygroundGridLayout
+          rank={gameRank}
+          data={playgroundCells}
+          onCellClick={onCellClick}
+          sellSize={ROW_MAX_HEIGHT / gameRank - (30 / gameRank)}
+          spacing={30 / gameRank}
+        />
+      </Center>
+
+      <Group spacing={5}>
+        <ActionIcon size={42} variant="default" onClick={onDecrementSize}>
+          –
+        </ActionIcon>
+        <NumberInput
+          hideControls
+          value={gameRank}
+          onChange={(val) => val && setGameRank(val)}
+          handlersRef={handlers}
+          max={12}
+          min={3}
+          step={1}
+          styles={{ input: { width: 54, height: 42, textAlign: 'center' } }}
+        />
+        <ActionIcon size={42} variant="default" onClick={onIncrementSize}>
+          +
+        </ActionIcon>
+      </Group>
+
+      <Button leftIcon={<Refresh/>} onClick={onResetClick}>
+        Сбросить
+      </Button>
+
+    </Stack>
+  </Container>
 }
 
 export default PlaygroundPage;
