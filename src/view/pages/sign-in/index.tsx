fef43cf0aafca77aff9d7ceaf97c58bf1fc50ecc
Vladimir <wlashcontact@gmail.com>
2022-10-14 21:26:13 +0300

    -- sign in, sign ip - pages - added

--- src/view/pages/sign-in/index.tsx
+++ src/view/pages/sign-in/index.tsx
@@ -1,6 +1,48 @@
+import React from "react";
+import {
+  Anchor,
+  Button,
+  Container,
+  Paper,
+  PasswordInput,
+  Text,
+  TextInput,
+  Title,
+} from "@mantine/core";
+import { useRouting } from "../../../core/hooks/use-routing";
+
+
 function SignInPage () {
-  return <>
-  </>
+
+  const routing = useRouting()
+
+  const onRegisterClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    event.preventDefault()
+    routing.go.to('signup')
+  }
+
+  const onLoginClick = () => {
+
+  }
+
+  return <Container size={420} my={40}>
+    <Title align='center'>
+      Добро пожаловать!
+    </Title>
+    <Text color='dimmed' size='sm' align='center' mt={5}>
+      Нет аккаунта?{' '}
+      <Anchor<'a'> href='signup' size='sm' onClick={onRegisterClick}>
+        Зарегистрироваться
+      </Anchor>
+    </Text>
+    <Paper withBorder shadow='md' p={30} mt={30} radius='md'>
+      <TextInput label='Имя пользователя' placeholder='Ваш никнейм' required />
+      <PasswordInput label='Пароль' placeholder='Ваш пароль' required mt='md' />
+      <Button fullWidth mt='xl' onClick={onLoginClick}>
+        Войти
+      </Button>
+    </Paper>
+  </Container>
 }
 
 export default SignInPage;
--- src/view/pages/sign-up/index.tsx
+++ src/view/pages/sign-up/index.tsx
@@ -1,6 +1,49 @@
+import React from "react";
+import {
+  Anchor,
+  Button,
+  Container,
+  Paper,
+  PasswordInput,
+  Text,
+  TextInput,
+  Title
+} from "@mantine/core";
+import { useRouting } from "../../../core/hooks/use-routing";
+
+
 function SignUpPage () {
-  return <>
-  </>
+
+  const routing = useRouting()
+
+  const onRegisterClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    event.preventDefault()
+    routing.go.root()
+  }
+
+  const onSignUpClick = () => {
+
+  }
+
+  return <Container size={420} my={40}>
+    <Title align='center'>
+      Регистрация
+    </Title>
+    <Text color='dimmed' size='sm' align='center' mt={5}>
+      Уже зарегистрированы?{' '}
+      <Anchor<'a'> href='signup' size='sm' onClick={onRegisterClick}>
+        Войти
+      </Anchor>
+    </Text>
+    <Paper withBorder shadow='md' p={30} mt={30} radius='md'>
+      <TextInput label='Имя' placeholder='Ваш никнейм' required mt='md' />
+      <TextInput label='Имя пользователя' placeholder='Ваш никнейм' required mt='md' />
+      <PasswordInput label='Пароль' placeholder='Ваш пароль' required mt='md' />
+      <Button fullWidth mt='xl' onClick={onSignUpClick}>
+        Зарегистрироваться
+      </Button>
+    </Paper>
+  </Container>
 }
 
 export default SignUpPage;
